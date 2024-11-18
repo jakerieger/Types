9208d8223d531417bf97db1054a7f2843bce2746
Jake Rieger <contact.jakerieger@gmail.com>
2024-11-18 04:14:16 -0500

    expect for option type

--- STL.h
+++ STL.h
@@ -13,72 +13,81 @@ using Array = std::array<T, N>;
 #endif
 
 #if defined(INC_ATOMIC)
-#include <atomic>
+    #include <atomic>
 template<typename T>
 using Atomic = std::atomic<T>;
 #endif
 
 #if defined(INC_BTREE)
-#include <map>
+    #include <map>
 template<typename K, typename V>
 using BTree = std::map<K, V>;
 #endif
 
 #if defined(INC_DICTIONARY)
-#include <unordered_map>
+    #include <unordered_map>
 template<typename K, typename V>
 using Dictionary = std::unordered_map<K, V>;
 #endif
 
 #if defined(INC_FUNCTION)
-#include <functional>
+    #include <functional>
 template<typename Sig>
 using Function = std::function<Sig>;
 #endif
 
 #if defined(INC_FUTURE)
-#include <future>
+    #include <future>
 template<typename T>
 using Future = std::future<T>;
 #endif
 
 #if defined(INC_LIST)
-#include <list>
+    #include <list>
 template<typename T>
 using List = std::list<T>;
 #endif
 
 #if defined(INC_MUTEX)
-#include <mutex>
+    #include <mutex>
 using Mutex = std::mutex;
 #endif
 
 #if defined(INC_OPTION)
-#include <optional>
+    #include <optional>
 template<typename T>
 using Option = std::optional<T>;
+
+template<typename T>
+T Expect(const Option<T>& opt, const std::string& msg) {
+    if (!opt.has_value()) {
+        std::cerr << "Expect: " << msg << std::endl;
+        std::exit(-10);
+    }
+    return opt.value();
+}
 #endif
 
 #if defined(INC_QUEUE)
-#include <queue>
+    #include <queue>
 template<typename T>
 using Queue = std::queue<T>;
 #endif
 
 #if defined(INC_PAIR)
-#include <utility>
+    #include <utility>
 template<typename V1, typename V2>
 using Pair = std::pair<V1, V2>;
 #endif
 
 #if defined(INC_TUPLE)
-#include <tuple>
+    #include <tuple>
 template<typename... Types>
 using Tuple = std::tuple<Types...>;
 #endif
 
 #if defined(INC_VECTOR)
-#include <vector>
+    #include <vector>
 template<typename T>
 using Vector = std::vector<T>;
 #endif
